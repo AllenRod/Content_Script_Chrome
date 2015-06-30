3a6fdb14dc7356f33583c3a2eb02ae2e0a20b903
AllenRod <lijiajie95@gmail.com>
2015-06-30 16:41:23 -0400

    Update core.js
    Add a function to show the image if URL leads to imgur.com.
    Resizing image is bugged, might try JQuery latter

--- core.js
+++ core.js
@@ -3,23 +3,39 @@ var newDiv = document.createElement('div');
 newDiv.className = 'core';
 var str = "";
 
-// Get the absolute path of the image 
+// Get the path of the check image
+var check = document.createElement("img");
+check.src = chrome.extension.getURL("images/check.png");
+check.height = "20";
+check.width = "20";
+
+// Create new image for imgur.com
 var img = document.createElement("img");
-img.src = chrome.extension.getURL("images/check.png");
-img.height = "20";
-img.width = "20";
 
 // Mouse listener for move over event on the current document.
 document.addEventListener('mouseover', function (event) {
-  // Check if the underlying element is a hyperlink
+  // Check if the underlying element is an <a>.
   if (event.target.nodeName == 'A') {
 	str = event.target.href;
 	// Check hostname
-	if ((/reddit.com/.test(str)) || (/imgur.com/.test(str)) ||
-	(/redd.it/.test(str)) || (/youtbe.com/.test(str)) || 
-	(/youtube.com/.test(str))) {
-		newDiv.innerHTML = "";
-		newDiv.appendChild(img);
+	if ((/reddit.com/.test(str)) || (/redd.it/.test(str)) 
+	|| (/youtu.be/.test(str)) || (/youtube.com/.test(str))
+	|| (/gfycat.com/.test(str))) {
+		newDiv.appendChild(check);
+	} else if (/imgur.com/.test(str)) {
+		// The URL leads to imgur.com
+		if ((/gallery/.test(str)) || (/.gifv\b/.test(str))
+		|| (/.webm\b/.test(str))) {
+			newDiv.appendChild(check);
+		} else {
+			if ((!(/.jpg\b/.test(str))) && (!(/.png\b/.test(str))) 
+			&& (!(/.gif\b/.test(str)))) {
+				str += ".jpg";
+			}
+			img.src = str;
+			imageMod(img);
+			newDiv.appendChild(img);
+		}
 	} else {
 		newDiv.innerHTML = str;
 	}
@@ -31,6 +47,16 @@ document.addEventListener('mouseover', function (event) {
 // Mouse listener for move out event on the current document
 document.addEventListener('mouseout', function(event) {
   if (event.target.nodeName == 'A') {
+	newDiv.innerHTML = "";
 	newDiv.style.visibility = 'hidden';
+	img.src = "";
   }
 });
+
+// Resize image 
+function imageMod(image) {
+	while ((image.height > 250) && (image.width > 250)) {
+		image.height *= 0.8;
+		image.width *= 0.8;
+	}
+}
