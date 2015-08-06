099f89e4fae1ad747b5d3663b6fe89edded96db8
AllenRod <lijiajie95@gmail.com>
2015-08-06 01:54:47 -0400

    Update imgprocess.js

--- imgprocess.js
+++ imgprocess.js
@@ -2,19 +2,29 @@
  * Display image when hovering over a link direct to http://imgur.com.
  * Process image with a <IMG> tag, and resize it to appropriate dimension.
  */
- 
+  
+// Create the image as the holder
+var img = $(document.createElement("img"));
+var frame = $(document.createElement("iframe"));
+frame.addClass("contaier");
+frame.attr("scrolling", "no", "allowfullscreen", "true",
+"height", "auto", "width", "auto");
+
 /**
  * Get the image from the URL and display with a <IMG> tag.
  * @param matchString	URL of the image
- * @param imgHolder		<IMG> tag to hold the image
  * @param ele			The hovering <A> tag
  */
-function imgProc(matchString, imgHolder, ele) {
+function imgProc(matchString, ele) {
 	if (/imgur.com/.test(matchString)) {
 		// The URL leads to imgur.com
-		if ((/gallery/.test(matchString)) || (/.webm\b/.test(matchString))
-		|| (/domain/.test(matchString)) || (/\/a\//.test(matchString))) {
-			newDiv.append(check);
+		if ((/.webm\b/.test(matchString))
+		|| (/domain/.test(matchString))) {
+			appendDiv(true);
+		} else if ((/gallery/.test(matchString)) || 
+		(/\/a\//.test(matchString))) {
+			//frame.attr("src", matchString);
+			//appendDiv(frame);
 		} else {
 			if (/.gifv\b/.test(matchString)) {
 				var n = matchString.indexOf(".gifv");
@@ -25,11 +35,11 @@ function imgProc(matchString, imgHolder, ele) {
 			&& (!(/.gif\b/.test(matchString)))) {
 				matchString += ".jpg";
 			}
-			imgHolder.attr("src", matchString);
-			imgHolder.load(function() {
-				resize(imgHolder);
+			img.attr("src", matchString);
+			img.load(function() {
+				resize(img);
 			});
-			appendDiv(imgHolder);
+			appendDiv(img);
 		}
 	}
 	append(ele);
@@ -48,4 +58,12 @@ function resize(image) {
 	}
 	image.height(h);
 	image.width(w);
+}
+
+/**
+ * Change <IMG> tag back to default setting.
+ */
+function imgClear() {
+	img.attr("src", "");
+	img.css("height", "auto", "width", "auto");
 }
