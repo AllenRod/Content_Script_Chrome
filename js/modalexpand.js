3aec5dcc208eb565c5d8a781bde60091483642de
AllenRod <lijiajie95@gmail.com>
2015-10-13 16:35:54 -0400

    Update modalexpand.js
    Add buttons to open page in new tab and close current modal

--- js/modalexpand.js
+++ js/modalexpand.js
@@ -2,13 +2,15 @@
  * Create a modal and display the page from URL with <IFRAME>.
  */
 // Append modal to the page 
-$('body').append('<div class="modal fade" id="modal-container"><div class="modal-dialog modal-lg" style="width:1000px;"><div class="modal-content"><div class="modal-body" style="padding:0px;"><iframe src="#" frameborder="0" allowfullscreen="" id="display-container"></iframe></div></div></div></div>');
+$('body').append('<div class="modal fade" id="modal-container"><div class="modal-dialog modal-lg" style="width:80%;"><div class="modal-content"><div class="modal-body" style="padding:0px;"><iframe src="#" frameborder="0" allowfullscreen="" id="display-container"></iframe></div></div><div class="modal-footer"><a id="open-window" href="" target="_blank"><button type="button" class="btn btn-primary" id="open-btn">Open</button></a><button type="button" class="btn btn-danger" data-dismiss="modal" style="margin-left:10px">Close</button></div></div></div>');
 
 /**
  * Set the 'src' attribute of <IFRAME> to the url, and display the modal.
  * @param urlString		The url of the page to be displayed
  */
 function expand(urlString) {
+	var h = $(window).height();
+	$("#display-container").height(h * 0.8);
 	if ((urlString.search(/youtube.com/) >= 0) || 
 	(urlString.search(/youtu.be/) >= 0)) {
 		urlString = embed(urlString);
@@ -24,4 +26,13 @@ function expand(urlString) {
  */
 $("#modal-container").on('hide.bs.modal', function(e) {
 	$("#display-container").attr('src', '');
+});
+
+/**
+ * Open a new window(tab) for the <IFRAME> page.
+ * Triggered by "Open" button.
+ */
+$("#open-btn").click( function() {
+	var url = $("#display-container").attr('src');
+	$("#open-window").attr('href', url);
 });
